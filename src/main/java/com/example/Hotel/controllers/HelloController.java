ae8a00e8609a82c80b692a1f62f16b94fb734851
Hozu Cosmin <hozu.cosmin@gmail.com>
2024-05-18 11:21:54 +0300

    Create project structure

--- src/main/java/com/example/Hotel/controllers/HelloController.java
+++ src/main/java/com/example/Hotel/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.Hotel;
+package com.example.Hotel.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
